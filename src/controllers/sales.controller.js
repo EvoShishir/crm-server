2964c05cab80cc3becc235e66a20067c656f3ca7
unknown <atefarmanshishir93@gmail.com>
2025-01-06 17:56:27 +0600

    added sales crud operation

--- src/controllers/sales.controller.js
+++ src/controllers/sales.controller.js
@@ -19,15 +19,58 @@ export const newSale = async (req, res, next) => {
   }
 };
 
-export const getSale = async (req, res, next) => {
+export const getAllSales = async (req, res, next) => {
   try {
     const salesData = await Sale.find();
     if (!salesData) {
-      return res.status(404).send("No data found");
+      return res.status(404).send("No sales data found");
     }
     res.status(200).send({
       success: true,
       salesData,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateSale = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { date, customer, serviceType, amount } = req.body;
+    const updatedSale = await Sale.findByIdAndUpdate(
+      id,
+      {
+        date: date,
+        customer: customer,
+        serviceType: serviceType,
+        amount: amount,
+      },
+      { new: true, runValidators: true }
+    );
+    if (!updateSale) {
+      return res.status(404).send("Sale not found");
+    }
+    res.status(200).send({
+      success: true,
+      updatedSale,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteSaleData = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const sale = await Sale.findByIdAndDelete(id);
+    if (!sale) {
+      return res.status(404).send("Sale not found");
+    }
+    res.status(200).send({
+      success: true,
+      message: "Deleted",
+      sale,
     });
   } catch (error) {
     next(error);
--- src/routes/sales.route.js
+++ src/routes/sales.route.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { getSale, newSale } from "../controllers/sales.controller.js";
+import {
+  deleteSaleData,
+  getAllSales,
+  newSale,
+  updateSale,
+} from "../controllers/sales.controller.js";
 import auth from "../middleware/auth.js";
 
 const salesRoutes = express.Router();
 
 salesRoutes.post("/new", auth, newSale);
-salesRoutes.get("/", auth, getSale);
+salesRoutes.get("/", auth, getAllSales);
+salesRoutes.patch("/:id", auth, updateSale);
+salesRoutes.delete("/:id", auth, deleteSaleData);
 
 export default salesRoutes;
